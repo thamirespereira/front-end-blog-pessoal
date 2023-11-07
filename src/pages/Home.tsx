9bd2b74108660a8e22c741b5a365c00a481edbc8
thamirespereira <thamiresfrpereira@hotmail.com>
2023-11-06 21:34:50 -0300

    Instalação e configuração do Tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <>
+            <div className="w-screen flex justify-center">
+                <div>
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui seus pensamentos e opniões</p>
+                    </div>
+
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
