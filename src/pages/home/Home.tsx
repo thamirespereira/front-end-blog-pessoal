75a8e89151116c622ad90bca733712b2163adcdb
thamirespereira <thamiresfrpereira@hotmail.com>
2023-11-07 11:24:35 -0300

    Adicionando a Context Api

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,15 @@
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Home() {
+    const { nome } = useContext(AuthContext)
     return (
+        
         <>
             <div className="bg-indigo-900 flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
-                        </h2>
+                    <h2 className='text-5xl font-bold'>Seja bem vinde, { nome }!</h2>
                         <p className='text-xl'>
                             Expresse aqui seus pensamentos e opniões
                         </p>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
+import { FormEvent, useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 function Login() {
 
-    return (
+    const { nome, trocarNome } = useContext(AuthContext)
+
+    const navigate = useNavigate()
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        navigate('/home')
+    }
+
+        return (
         <>
             <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4" >
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
+                <form className="flex justify-center items-center flex-col w-1/2 gap-4" 
+                    onSubmit={handleSubmit}>
+                    <h2 className="text-slate-900 text-5xl">Entrar</h2>
                     <div className="flex flex-col w-full">
                         <label htmlFor="usuario">Usuário</label>
                         <input
@@ -16,18 +28,8 @@ function Login() {
                             name="usuario"
                             placeholder="Usuario"
                             className="border-2 border-slate-700 rounded p-2"
-
-                        />
-                    </div>
-                    <div className="flex flex-col w-full">
-                        <label htmlFor="senha">Senha</label>
-                        <input
-                            type="password"
-                            id="senha"
-                            name="senha"
-                            placeholder="Senha"
-                            className="border-2 border-slate-700 rounded p-2"
-
+                            value={nome}
+                            onChange={(event) => trocarNome(event.target.value)}
                         />
                     </div>
                     <button
