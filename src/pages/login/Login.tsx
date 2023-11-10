726f59686ef4db7bb18bedeff02cdc8d7ee71213
thamirespereira <thamiresfrpereira@hotmail.com>
2023-11-10 20:46:49 -0300

    Estilizando a página Login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -34,38 +34,38 @@ function Login() {
 
         return (
         <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4" 
+            <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold bg-teal-200">
+                <form className="flex justify-center items-center flex-col w-1/2 gap-4 max-w-sm m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl" 
                     onSubmit={login}>
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
+                    <h2 className="text-white-600 text-5xl font-sans">Entrar</h2>
                     <div className="flex flex-col w-full">
-                        <label htmlFor="usuario">Usuário</label>
+                        <label htmlFor="usuario font-sans">Usuário</label>
                         <input
                             type="text"
                             id="usuario"
                             name="usuario"
-                            placeholder="Usuario"
-                            className="border-2 border-slate-700 rounded p-2"
+                            placeholder="Digite seu email"
+                            className="border-2 bg-white rounded p-2 "
                             value={usuarioLogin.usuario}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
                     <div className="flex flex-col w-full">
-                        <label htmlFor="senha">Senha</label>
+                        <label htmlFor="senha font-sans">Senha</label>
                         <input
                             type="password"
                             id="senha"
                             name="senha"
-                            placeholder="Senha"
-                            className="border-2 border-slate-700 rounded p-2"
+                            placeholder="Digite sua senha"
+                            className="border-2 bg-white rounded p-2"
                             value={usuarioLogin.senha} 
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
                     <button
                         type='submit'
-                        className="rounded bg-indigo-400 flex justify-center
-                                   hover:bg-indigo-900 text-white w-1/2 py-2">
+                        className="rounded bg-gray-700 flex justify-center
+                                   hover:bg-gray-300 text-white w-1/2 py-2">
                         {isLoading ? <RotatingLines
                             strokeColor="white"
                             strokeWidth="5"
@@ -80,7 +80,7 @@ function Login() {
 
                     <p>
                         Ainda não tem uma conta?{' '}
-                        <Link to="/cadastro" className="text-indigo-800 hover:underline">
+                        <Link to="/cadastro" className="gray-500 hover:text-red-400">
                             Cadastre-se
                         </Link>
                     </p>
