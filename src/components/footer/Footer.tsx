fa4499a45a2a80095b5db126421f96d58629fc4c
thamirespereira <thamiresfrpereira@hotmail.com>
2023-11-16 15:15:00 -0300

    Alterando a exibição do Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,18 @@
-import { FacebookLogo, GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
             <div className="flex justify-center bg-black text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
@@ -18,6 +25,11 @@ function Footer() {
                     </div>
                 </div>
             </div>
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
